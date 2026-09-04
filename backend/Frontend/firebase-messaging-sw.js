ef30b3a6f707dddc10ec48e9c953460db2a25c5f
timosof <129143827+timosof@users.noreply.github.com>
2026-09-04 10:46:02 +0100

    Update firebase-messaging-sw.js

--- backend/Frontend/firebase-messaging-sw.js
+++ backend/Frontend/firebase-messaging-sw.js
@@ -1,13 +1,1 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.1/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDpS6VQdXbgEhNyCpyvM7tnFezSIcTnpYs",
-  authDomain: "shopbygold-b32af.firebaseapp.com",
-  projectId: "shopbygold-b32af",
-  storageBucket: "shopbygold-b32af.firebasestorage.app",
-  messagingSenderId: "749710700838",
-  appId: "1:749710700838:web:49051875f7891ef99611e7"
-});
-
-const messaging = firebase.messaging();
